deb9c4c9b86c2bef84de58cfc08a48998d5ae727
Robert Parr <rp@fastmail.co.uk>
2016-02-25 16:06:07 +0000

    Updated publisher mock test with labels

--- src/test/groovy/PublisherSpec.groovy
+++ src/test/groovy/PublisherSpec.groovy
@@ -3,19 +3,22 @@ import spock.test.*;
 class PublisherSpec extends spock.lang.Specification {
 
   def "events are received by all subscribers"() {
-  	setup:
-	def subscriber1 = Mock(Subscriber)
-	def subscriber2 = Mock(Subscriber)
+  	setup: "mock a couple subscribers"
+		def subscriber1 = Mock(Subscriber)
+		def subscriber2 = Mock(Subscriber)
 
-	def publisher = new Publisher()
-	publisher.add(subscriber1)
-	publisher.add(subscriber2)
+	and: "create publisher"
+		def publisher = new Publisher()
+	
+	and: "add subscribers to publisher"
+		publisher.add(subscriber1)
+		publisher.add(subscriber2)
   	
     when:
-    publisher.fire("Abid")
+	    publisher.fire("Abid")
     
-    then:
-	1 * subscriber1.eventReceived("Abid")
-  	1 * subscriber2.eventReceived("Abid")
+    then: "all subscribers should receive an event once"
+		1 * subscriber1.eventReceived("Abid")
+	  	1 * subscriber2.eventReceived("Abid")
   }
 }  
